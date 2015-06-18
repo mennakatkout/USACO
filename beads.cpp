77920a8101c334912ace0190b7d470384ed5c33e
mennakatkout <menna.katkout@gmail.com>
2015-06-18 08:00:04 +0200

    - adding second problem in usaco

---
+++ beads.cpp
@@ -0,0 +1,19 @@
+/*
+ * beads.cpp
+ *
+ *  Created on: Jun 18, 2015
+ *      Author: HPKT3764
+ */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string str;
+	int len;
+	cin>>len>>str;
+
+
+	return 0;
+}
